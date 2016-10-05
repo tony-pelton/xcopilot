8ffc6ebc53d8cd0418aad56a87753edc2f6a128c
Tony Pelton <tpelton@gmail.com>
2016-10-04 22:07:01 -0400

    configure a non-default task scheduler and an async event bus

--- src/main/java/com/dsrts/xcopilot/XcopilotApplication.java
+++ src/main/java/com/dsrts/xcopilot/XcopilotApplication.java
@@ -1,5 +1,7 @@
 package com.dsrts.xcopilot;
 
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import javax.swing.*;
 
@@ -35,5 +38,21 @@ public class XcopilotApplication {
         return (strings) -> {
             SwingUtilities.invokeLater(() -> {main.pack();main.setSize(1024,768);main.setVisible(true);});
         };
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setWaitForTasksToCompleteOnShutdown(true);
+        threadPoolTaskScheduler.setPoolSize(10);
+        threadPoolTaskScheduler.setDaemon(true);
+        threadPoolTaskScheduler.setThreadNamePrefix("xcopilot-");
+        return threadPoolTaskScheduler;
+    }
+
+    @Bean
+    public EventBus eventBus(ThreadPoolTaskScheduler taskScheduler) {
+        AsyncEventBus asyncEventBus = new AsyncEventBus(taskScheduler.getScheduledExecutor());
+        return asyncEventBus;
     }
 }
